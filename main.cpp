b973e7c5b4167d46d045c2efbcbeda0d0889acdf
Ant0CPU <antonio.bruno@marconibari.edu.it>
2024-10-31 09:09:24 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,35 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int i;
+    float lvl, somma, media;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    somma=0;
+
+    for (i=1; i<=7; i++)
+    {
+        cout<<"Inserire il livello dell'acqua"<<endl;
+        cin>>lvl;
+
+        if (lvl>10)
+        {
+        
+        cout<<"Nel giorno " <<i<< " l'acqua è alta"<<endl;
+
+        }
+
+        somma=lvl+somma;
+    }
+
+    media=somma/7;
+
+    if (media>0)
+    {
+        cout<<"Acqua alta"<<endl;
+    }
+
+    if (media<=0)
+    {
+        cout<<"Livello normale"<<endl;
+    }
+}
